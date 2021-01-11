d1ff5d70e7268258f991bc237aadac3f469eb355
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2021-01-11 00:26:40 -0300

    Finish projects in golang from course

--- go-webapi/controllers/produtos.go
+++ go-webapi/controllers/produtos.go
@@ -18,17 +18,45 @@ func New(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "New", nil)
 }
 
+func Edit(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	product := models.Find(id)
+	temp.ExecuteTemplate(w, "Edit", product)
+}
+
 func Insert(w http.ResponseWriter, r *http.Request) {
 	nome := r.FormValue("nome")
 	descricao := r.FormValue("descricao")
 	preco, _ := strconv.ParseFloat(r.FormValue("preco"), 64)
 	quantidade, _ := strconv.Atoi(r.FormValue("quantidade"))
 
-	result, err := models.SaveProduto(models.Produto{Nome: nome, Descricao: descricao, Preco: preco, Quantidade: quantidade})
+	result, err := models.Save(models.Produto{Nome: nome, Descricao: descricao, Preco: preco, Quantidade: quantidade})
 
 	if err != nil {
 		temp.ExecuteTemplate(w, "New", err.Error())
 	}
+
+	if result {
+		http.Redirect(w, r, "/", 301)
+	}
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	models.RemoveProduct(id)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	nome := r.FormValue("nome")
+	descricao := r.FormValue("descricao")
+	preco, _ := strconv.ParseFloat(r.FormValue("preco"), 64)
+	quantidade, _ := strconv.Atoi(r.FormValue("quantidade"))
+
+	result, _ := models.Update(models.Produto{Id: id, Nome: nome, Descricao: descricao, Preco: preco, Quantidade: quantidade})
 
 	if result {
 		http.Redirect(w, r, "/", 301)
--- go-webapi/models/produto.go
+++ go-webapi/models/produto.go
@@ -51,7 +51,7 @@ func FindAllProdutos() []Produto {
 	return produtos
 }
 
-func SaveProduto(p Produto) (bool, error) {
+func Save(p Produto) (bool, error) {
 	db := repository.ConectaDb()
 	defer db.Close()
 
@@ -66,4 +66,58 @@ func SaveProduto(p Produto) (bool, error) {
 	}
 
 	return false, err
+}
+
+func Update(p Produto) (bool, error) {
+	db := repository.ConectaDb()
+	defer db.Close()
+
+	fmt.Println("Id atualizado", p.Id)
+	result, err := db.Exec("UPDATE produtos SET nome=$1, descricao=$2, preco=$3, quantidade=$4 WHERE id = $5",
+		p.Nome, p.Descricao, p.Preco, p.Quantidade, p.Id)
+
+	if err == nil {
+		if rows, _ := result.RowsAffected(); rows > 0 {
+			fmt.Println("Produto Atualizado")
+		}
+		return true, nil
+	}
+
+	return false, err
+}
+
+func Find(id int) Produto {
+	db := repository.ConectaDb()
+	defer db.Close()
+
+	rows, err := db.Query("select * from produtos where id = $1", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produto := Produto{}
+	for rows.Next() {
+		var nome, descricao string
+		var id, quantidade int
+		var preco float64
+
+		rows.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		produto.Id = id
+		produto.Nome = nome
+		produto.Descricao = descricao
+		produto.Preco = preco
+		produto.Quantidade = quantidade
+	}
+	return produto
+}
+
+func RemoveProduct(id int) (bool, error) {
+	db := repository.ConectaDb()
+
+	result, err := db.Exec("DELETE FROM produtos where id = $1", id)
+
+	row, _ := result.RowsAffected()
+	defer db.Close()
+	return row > 0, err
 }
